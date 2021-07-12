e650888e9dea4c513999a1e8dca79c93ad67b2e0
satoshishi <s9r6nbij9@gmail.com>
2021-07-12 19:27:58 +0900

    asmdfを定義

--- main/Assets/StateMachineCore/Strategy/IStateMachineStrategy.cs
+++ main/Assets/StateMachineCore/Strategy/IStateMachineStrategy.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using System.Linq;
-using StatMachine.Observer;
 
 namespace StatMachine.Strategy
 {
     public interface IStateMachineStrategy<STATE_NODE> : IDisposable
     {
         IEnumerable<STATE_NODE> StateNodes{get;}
-
-        //container
-
         void GoTo<T>() where T : STATE_NODE;
-        
         void GoTo(Type state);
     }
 }
